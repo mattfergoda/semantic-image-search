2d07c6e5f409d922134da30a28fd7e0b878e72dd
Matt Fergoda <mattfergoda@users.noreply.github.com>
2024-01-30 16:40:24 -0800

    Fix spacing.

--- auth.py
+++ auth.py
@@ -2,10 +2,10 @@ import os
 
 from passlib.context import CryptContext
 
-
 ADMIN_PW = os.environ.get("ADMIN_PW")
 
 pw_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def verify_admin(hashed_password):
     """Verify that the plain password hashes to the correct admin password."""
