275ccf17c7f7c394bb2f17c2c2604aaae170e9f8
Matt Fergoda <mattfergoda@users.noreply.github.com>
2024-02-01 09:57:46 -0800

    Test get images, create image, delete image.

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -6,6 +6,7 @@ from pgvector.sqlalchemy import Vector
 
 from database import Base
 from models import Image, EMBEDDING_SIZE, Vector
+import schemas
 import crud
 
 SQLALCHEMY_TEST_DATABASE_URL = "postgresql:///semantic_pic_test"
@@ -18,37 +19,87 @@ TestingSessionLocal = sessionmaker(
     bind=engine
 )
 
-@pytest.fixture
-def session():
-    Base.metadata.create_all(bind=engine)
-    session = TestingSessionLocal()
 
-    images = [
-        Image(
-        name="test1", 
-        aws_image_src="https://test1.jpg", 
-        exif_data={"field1" : "value1"},
-        embedding = np.random.random(EMBEDDING_SIZE).tolist()),
-        Image(
-        name="test2", 
-        aws_image_src="https://test2.jpg", 
-        exif_data={"field1" : "value1"},
-        embedding = np.random.random(EMBEDDING_SIZE).tolist())
-    ]
-    session.bulk_save_objects(images)
-    session.commit()
+class TestCRUD:
+    @pytest.fixture
+    def session(self):
+        Base.metadata.create_all(bind=engine)
+        session = TestingSessionLocal()
 
-    yield session
+        self.image_1 = Image(
+            name="test1", 
+            aws_image_src="https://test1.jpg", 
+            exif_data={"field1" : "value1"},
+            embedding = np.random.random(EMBEDDING_SIZE).tolist()
+        )
+        self.image_2 = Image(
+            name="test2", 
+            aws_image_src="https://test2.jpg", 
+            exif_data={"field1" : "value1"},
+            embedding = np.random.random(EMBEDDING_SIZE).tolist()
+        )
+        self.images = [self.image_1, self.image_2]
+        session.bulk_save_objects(self.images)
+        session.commit()
 
-    session.close()
-    Base.metadata.drop_all(bind=engine)
+        yield session
 
-def test_get_image(session: Session):
-    """Test getting a single image."""
-    
-    image = crud.get_image(session, "test1")
-    assert image.name == "test1"
-    assert image.aws_image_src == "https://test1.jpg"
-    assert image.exif_data == {"field1" : "value1"}
-    assert isinstance(image.embedding, np.ndarray)
-    assert len(image.embedding) == EMBEDDING_SIZE
+        session.close()
+        Base.metadata.drop_all(bind=engine)
+
+    def test_get_image(self, session: Session):
+        """Test getting a single image."""
+        
+        image = crud.get_image(session, "test1")
+        assert image.name == "test1"
+        assert image.aws_image_src == "https://test1.jpg"
+        assert image.exif_data == {"field1" : "value1"}
+        assert isinstance(image.embedding, np.ndarray)
+        assert len(image.embedding) == EMBEDDING_SIZE
+
+    def test_get_images(self, session: Session):
+        """Test getting multiple images."""
+
+        image_names = [i.name for i in self.images]
+        images = crud.get_images(session, search_term="foo")
+
+        for image in images:
+            assert image.name in image_names
+            assert isinstance(image.aws_image_src, str)
+            assert isinstance(image.exif_data, dict)
+            assert isinstance(image.embedding, np.ndarray)
+
+    def test_create_image(self, session: Session):
+        """Test creating an image."""
+
+        image = schemas.ImageCreate(
+            name="image3",
+            aws_image_src="https://test3.jpg",
+            exif_data={"field1" : "value1"},
+            embedding=np.random.random(EMBEDDING_SIZE).tolist()
+        )
+        crud.create_image(session, image)
+
+        db_image = crud.get_image(session, image.name)
+
+        assert db_image.name == image.name
+        assert db_image.aws_image_src == image.aws_image_src
+        assert db_image.exif_data == image.exif_data
+        assert isinstance(db_image.embedding, np.ndarray)
+        assert len(db_image.embedding) == EMBEDDING_SIZE
+
+    def test_delete_image(self, session: Session):
+        """Test deleting an image."""
+
+        image_name = self.image_1.name
+        image_1 = crud.get_image(session, image_name)
+
+        assert image_1 is not None
+
+        crud.delete_image(session, image_name)
+        image_none = crud.get_image(session, image_name)
+
+        assert image_none is None
+
+        
+
