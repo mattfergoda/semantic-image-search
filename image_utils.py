b4545f755fd10857ecff9baee3b1ffff0b7d1bc9
Matt Fergoda <mattfergoda@users.noreply.github.com>
2024-02-02 11:01:30 -0800

    Remove unneccessary else block

--- image_utils.py
+++ image_utils.py
@@ -7,26 +7,22 @@ def scrape_exif(image):
     Returns a dict containing EXIF image metadata:
     { tag_name : tag_value }
     """
-    # img = Image.open("sample.jpg")
-    # img_exif = img.getexif()
 
     img = Image.open(image)
     img_exif = img.getexif()
 
     metadata = {}
-    if img_exif is None:
-        return metadata
-    else:
-        for key, val in img_exif.items():
-            if key in ExifTags.TAGS:
-                if isinstance(val, (TiffImagePlugin.IFDRational)):
-                    metadata[ExifTags.TAGS[key]] = float(val)
-                elif isinstance(val, (bytes)):
-                    pass
-                else:
-                    metadata[ExifTags.TAGS[key]] = val
+
+    for key, val in img_exif.items():
+        if key in ExifTags.TAGS:
+            if isinstance(val, (TiffImagePlugin.IFDRational)):
+                metadata[ExifTags.TAGS[key]] = float(val)
+            elif isinstance(val, (bytes)):
+                pass
             else:
-                print(f'{key}:{val}')
-                metadata[key] = val
+                metadata[ExifTags.TAGS[key]] = val
+        else:
+            print(f'{key}:{val}')
+            metadata[key] = val
 
     return metadata
